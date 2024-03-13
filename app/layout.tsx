a31970e66fbe88e2a4fca4dc8a3a25521490b85f
headstartlabs <headstart.labs@gmail.com>
2024-03-13 17:13:25 +1100

    Fix backgroudn maybe

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
-
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -21,6 +21,9 @@ export default function RootLayout({
       lang="en"
       className={`${GeistSans.variable} ${GeistMono.variable} bg-light_grey_p`}
     >
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,27 @@ import { motion } from "framer-motion";
 export default function Home() {
   return (
     <main className="relative min-h-screen p-24">
-      <div className="absolute inset-x-0 left-0 right-0 mx-auto max-w-[1600px] ">
-        <motion.div
-          key="22"
-          initial={{ opacity: 0 }} // Initial opacity before animation
-          animate={{ opacity: 1 }} // Opacity when animation completes
-          transition={{ duration: 0.5 }} // Animation duration and delay
-        >
-          <div className="flex flex-col items-center justify-center text-center">
-            <div className={"text-8xl text-yellow_p"}>Bento Box</div>
+      <div className="flex flex-col items-center text-center">
+        <div className="absolute inset-x-0 left-0 right-0 mx-auto max-w-[1600px] ">
+          <motion.div
+            key="22"
+            initial={{ opacity: 0 }} // Initial opacity before animation
+            animate={{ opacity: 1 }} // Opacity when animation completes
+            transition={{ duration: 0.5 }} // Animation duration and delay
+          >
+            <div className="flex flex-col items-center justify-center text-center">
+              <div className={"text-8xl text-yellow_p"}>Bento Box</div>
 
-            <div className={"text-xl mb-5 text-red_p text-center"}>
-              By: Andrew Do
+              <div className={"text-xl mb-5 text-red_p text-center"}>
+                By: Andrew Do
+              </div>
             </div>
-          </div>
-        </motion.div>
-        <Grid />
-        <div className="flex flex-col items-center justify-center px-5 text-ali">
-          <div className={"text-xl my-10 text-yellow_p text-center"}>
-            "An unconventional portfolio of works."
+          </motion.div>
+          <Grid />
+          <div className="flex flex-col items-center justify-center px-5 text-ali">
+            <div className={"text-xl my-10 text-yellow_p text-center"}>
+              "An unconventional portfolio of works."
+            </div>
           </div>
         </div>
       </div>
