4af5f63a65c31b5a62708cb2bc4a670ed53f4cc3
headstartlabs <headstart.labs@gmail.com>
2024-04-24 22:16:56 +1000

    Fix background color

--- components/email/email.tsx
+++ components/email/email.tsx
@@ -43,7 +43,7 @@ const Email = () => {
           Want to reach out?
         </h2>
         <button
-          className="bg-white dark:bg-gray-700 hover:bg-red_p text-dark_grey_p dark:text-white text-sm hover:text-white px-4 py-2 rounded-full transition-colors duration-300"
+          className="bg-white dark:bg-zinc-700 hover:bg-red_p text-dark_grey_p dark:text-white text-sm hover:text-white px-4 py-2 rounded-full transition-colors duration-300"
           onClick={handleSendEmail}
           onTouchStart={handleTouchStart}
         >
